b9f0ac2f946942477a7188e6bf4478e286c13756
Deesh <internetwasmyidea@gmail.com>
2020-01-19 19:35:19 +0530

    weather app is working now ready to be hosted (the api key which i used is sample need to change once that got successfully hosted)

---
+++ weather/views.py
@@ -0,0 +1,39 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+        # city_weather = {
+        #     'city' : "kadapa",
+        #     'temperature' : "102",
+        #     'description' : "this is sample data",
+        #     'icon' : "j.png",
+        # }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/weather.html', context)
